8397f17f849c0b1311898a8bb1d5b0b1bba7ca46
Mweya Ruider <mweyaruider@gmail.com>
2019-04-03 07:43:47 +0200

    Added the add method to the linked list

--- StarLAN/src/starlan/misc/LinkedList.java
+++ StarLAN/src/starlan/misc/LinkedList.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* These classes define the linked list which stores information */
 package starlan.misc;
 
 /**
  *
  * @author mweya
  */
-public class LinkedList {
+public class LinkedList<AnyType> {
+    private Node<AnyType> head = null;
+    
+    public LinkedList() {}
+    
+    public LinkedList(Node head) {
+        this.head = head;
+    }
+    
+    public LinkedList(AnyType data) {
+        this.head = new Node<AnyType>(data);
+    }
+    
+    public void add(AnyType data) {
+        Node toadd = new Node<AnyType>(data);
+        add(toadd);
+    }
+    
+    public void  add(Node toadd) {
+        if (head == null) {
+            this.head = toadd;
+        } else {
+            Node currentNode = head;
+            while (currentNode.getNext() != null) {
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setNext(toadd);
+        }
+    }
+    
     
 }
