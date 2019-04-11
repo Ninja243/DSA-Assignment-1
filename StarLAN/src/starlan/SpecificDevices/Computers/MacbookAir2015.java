31fa79075cace0bec81971424830250b4ef5df3d
Mweya Ruider <mweyaruider@gmail.com>
2019-04-11 13:21:10 +0200

    Added specific device classes (No delays yet)

---
+++ StarLAN/src/starlan/SpecificDevices/Computers/MacbookAir2015.java
@@ -0,0 +1,16 @@
+/* This is an example of how we can simulate specific devices. The device being specified here is a Macbook Air (2015) laptop */
+package starlan.SpecificDevices.Computers;
+
+/**
+ *
+ * @author mweya
+ */
+import starlan.GenericClasses.Device;
+import starlan.GenericClasses.serverNode;
+public class MacbookAir2015<AnyType> extends Device{
+    
+    // We assume that the laptop will send and recieve messages slightly slower
+    // than a computer would be able to do, therefore a delay is added
+    
+     
+}
---
+++ StarLAN/src/starlan/SpecificDevices/Phones/SamsungGalaxyS3.java
@@ -0,0 +1,16 @@
+/* This is an example of how we can simulate specific devices. The device being specified here is a Samsung Galaxy S3 (i9300) phone */
+package starlan.SpecificDevices.Phones;
+
+/**
+ *
+ * @author mweya
+ */
+import starlan.GenericClasses.Device;
+import starlan.GenericClasses.serverNode;
+public class SamsungGalaxyS3<AnyType> extends Device{
+    
+    // We assume that the phone will send and recieve messages slower
+    // than a computer would be able to do, therefore a delay is added
+    
+     
+}
