aed925825774002dd800edb56019afde5f1c8c3c
Mweya Ruider <mweyaruider@gmail.com>
2019-04-12 22:11:15 +0200

    Added the names of the group members to the main file and removed all the license headers I could find

--- StarLAN/src/starlan/GenericClasses/clientNode.java
+++ StarLAN/src/starlan/GenericClasses/clientNode.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* This class describes how a client should act */
 package starlan.GenericClasses;
 
 import starlan.ErrorClasses.InvalidAddressException;
